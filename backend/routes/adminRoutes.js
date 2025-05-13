865d2d62c598ce0835fd538fea9ca08f702d539c
Akashine-012 <akashiney.22cse@kongu.edu>
2025-05-13 23:12:15 +0530

    Backend

--- backend/routes/adminRoutes.js
+++ backend/routes/adminRoutes.js
@@ -1,12 +1,11 @@
 const express = require("express");
 const { registerAdmin, loginAdmin, adminDashboard, getPendingExpenses, approveExpense, rejectExpense, getAllExpenseStats, getExpenseHistory, addIncome, getAllIncomes, getFinancialSummary } = require("../controllers/adminController");
-const authMiddleware = require("../middleware/authMiddleware");
 
 const router = express.Router();
 
 router.post("/register", registerAdmin);
 router.post("/login", loginAdmin);
-router.get("/dashboard", authMiddleware, adminDashboard);
+router.get("/dashboard", adminDashboard);
 router.get("/pending-expenses", getPendingExpenses);
 router.post("/approve-expense/:id", approveExpense);
 router.post("/reject-expense/:id", rejectExpense);
